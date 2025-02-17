508c9deb02f0da44370fcdaba2708229078483b8
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2025-02-17 13:23:45 +0700

    basic routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello',function(){
+    return 'Hello World';
+});
+
+Route::get('/world',function(){
+    return 'World';
+});
+
+Route::get('/',function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/about',function(){
+    return '2341720122 Rahmad Dwi Ferdyan';
+});
+
+Route::get('/user/{name}', function ($name) { 
+    return 'Nama saya '.$name; 
+}); 
