b4b9ba5fe720a842f72ffa5d6d7b84125602f6de
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2025-02-17 13:53:20 +0700

    route parameters

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,11 @@ Route::get('/about',function(){
 Route::get('/user/{name}', function ($name) { 
     return 'Nama saya '.$name; 
 }); 
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+});
+
+Route::get('/articles/{id}', function($id) {
+    return 'Halaman artikel dengan ID ' . $id;
+});
