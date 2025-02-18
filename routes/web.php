2d591da22c87e907534b4d3b02424619da9039c6
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2025-02-18 17:11:34 +0700

    membuat controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\WelcomeController;
+
+
+use App\Http\Controllers\PageController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,21 +24,21 @@ Route::get('/', function () {
 });
 
 // BASIC ROUTING
-Route::get('/hello',function(){
-    return 'Hello World';
-});
+// Route::get('/hello',function(){
+//     return 'Hello World';
+// });
 
-Route::get('/world',function(){
-    return 'World';
-});
+// Route::get('/world',function(){
+//     return 'World';
+// });
 
-Route::get('/',function(){
-    return 'Selamat Datang';
-});
+// Route::get('/',function(){
+//     return 'Selamat Datang';
+// });
 
-Route::get('/about',function(){
-    return '2341720122 Rahmad Dwi Ferdyan';
-});
+// Route::get('/about',function(){
+//     return '2341720122 Rahmad Dwi Ferdyan';
+// });
 
 // PARAMETERS ROUTING
 // Route::get('/user/{name}', function ($name) { 
@@ -43,9 +49,9 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
 });
 
-Route::get('/articles/{id}', function($id) {
-    return 'Halaman artikel dengan ID ' . $id;
-});
+// Route::get('/articles/{id}', function($id) {
+//     return 'Halaman artikel dengan ID ' . $id;
+// });
 
 // OPTIONAL PARAMETERS
 // Route::get('/user/{name?}', function ($name='John'){
@@ -82,6 +88,10 @@ Route::get('/user/profile', function(){
 //     Route::get('/event', [EventController::class, 'index']);
 // });
 
+Route::get('/hello', [WelcomeController::class, 'hello']);
 
 
+Route::get('/', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
 
