5015b10bb9a536b0870f7b364e898200ce27ed1d
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2025-02-18 16:32:17 +0700

    fixed

--- routes/web.php
+++ routes/web.php
@@ -48,9 +48,40 @@ Route::get('/articles/{id}', function($id) {
 });
 
 // OPTIONAL PARAMETERS
-Route::get('/user/{name?}', function ($name='John'){
-    return 'Nama saya ' . $name;
-});
+// Route::get('/user/{name?}', function ($name='John'){
+//     return 'Nama saya ' . $name;
+// });
+
+//ROUTE NAME
+Route::get('/user/profile', function(){
+    //
+}) ->name('profile');
+
+// Route::get('/user/makan', function(){
+//     return redirect()->route('profile');
+// });
+
+
+// //ROUTE GROUPING
+// Route::middleware(['first', 'second'])->group(function () {
+//     Route::get('/', function () {
+//         // Uses first & second middleware...
+//     });
+//     Route::get('/user/profile', function () {
+//         // Uses first & second middleware...
+//     });
+// });
+// Route::domain('{account}.example.com')->group(function () {
+//     Route::get('user/{id}', function ($account, $id) {
+//         //
+//     });
+// });
+// Route::middleware('auth')->group(function () {
+//     Route::get('/user', [UserController::class, 'index']);
+//     Route::get('/post', [PostController::class, 'index']);
+//     Route::get('/event', [EventController::class, 'index']);
+// });
+
 
 
 
